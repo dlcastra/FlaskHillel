5ad1e8d2a5de1ee362756e8e25320f01a5376e75
dlcastra <1dlcastra@gmail.com>
2023-09-22 15:01:02 +0200

    added three new queries

--- flask_practice.py
+++ flask_practice.py
@@ -1,11 +1,16 @@
-from flask import Flask, send_file
-from faker import Faker
-from setups import setup_customers, setup_tracks
 import csv
+import sqlite3
+
 import requests
+from faker import Faker
+from flask import Flask, send_file, render_template
+
+from setups import setup_customers, setup_tracks
 
 
 app = Flask(__name__)
+
+""" -- Головна сторінка і генератори -- """
 
 
 @app.route('/')
@@ -17,12 +22,29 @@ def main_page():
         ", щоб згенерувати певну кількість людей та їх email-и, за замовчуванням це буде 100"
         "<p>Напишіть у пошуковий рядок /mean/, щоб побачити середній зріст та вагу з файлу hw.csw"
         "<p>Напишіть у пошуковий рядок /space/, щоб побачити кількість космонафтів"
+        "<p>Напишіть у пошуковий рядок /setup/, щоб згенерувати дані для бази даних"
+        "<p>Напишіть у пошуковий рядок /first_name/, щоб побачити кількість імен і самі імена в базі даних"
+        "<p>Напишіть у пошуковий рядок /tracks/, щоб побачити кількість треків в базі даних"
+        "<p>Напишіть у пошуковий рядок /tracks-sec/, щоб побачити інформацію о треках"
+
     )
+
+
+@app.route('/setup/')
+def setups():
+    setup_customers()
+    setup_tracks()
+
+    return "Ви згенерували дані для таблиці customers і tracks в біза даних flask"
+
+
+""" -- Перша домашка по flask -- """
 
 
 @app.route('/requrements/', methods=['GET'])
 def requrements():
-    return send_file('tables_and_txt/requrements.txt', as_attachment=False)  # Висилаємо файл так, щоб його не пропонувало завантажити
+    # Висилаємо файл так, щоб його не пропонувало завантажити
+    return send_file('tables_and_txt/requrements.txt', as_attachment=False)
 
 
 @app.route('/users/generate/<int:generate>', methods=['GET'])
@@ -70,10 +92,49 @@ def cosmonaut_count():
     numbers_cosmonaut = response_in_json.get('number')  # Беремо кількість космонафтів з json файлу
 
     names = []
-    for person in response_in_json.get('people'):  # Беремо всі імена космонафтів, щоб потім перевірити їх кількість ще раз
+    for person in response_in_json.get('people'):
+        # Беремо всі імена космонафтів, щоб потім перевірити їх кількість ще раз
         names.append(person['name'])
 
     return f"Вказана кількість космонавтів {numbers_cosmonaut}, кількість після перевірки {len(names)}"
+
+
+""" -- Друга домашка flask + SQLite"""
+
+
+@app.route('/first_name/', methods=['GET'])
+def get_customer_name():
+    with sqlite3.connect('flask.db') as conn:
+        cur = conn.cursor()
+        cur.execute(""" SELECT first_name FROM customers """)
+        customers = cur.fetchall()
+        names_counter = len(customers)
+
+        return render_template('customers.html', names_counter=names_counter, customers=customers)
+
+
+@app.route('/tracks/', methods=['GET'])
+def get_count_records_tracks():
+    with sqlite3.connect('flask.db') as conn:
+        cur = conn.cursor()
+        cur.execute(""" SELECT id FROM tracks """)
+        tracks = cur.fetchall()
+        tracks_counter = len(tracks)
+
+        return render_template('count_traks.html', tracks_counter=tracks_counter, tracks=tracks)
+
+
+@app.route('/tracks-sec/', methods=['GET'])
+def get_tracks_information():
+    with sqlite3.connect('flask.db') as conn:
+        cur = conn.cursor()
+        cur.execute(""" SELECT id, singer, track_name, track_length, release_date FROM tracks """)
+        tracks = cur.fetchall()
+
+        return render_template('tracks_information.html',tracks=tracks)
+
+
+""" --  Чатина для помилок -- """
 
 
 @app.errorhandler(404)
@@ -82,6 +143,4 @@ def error_404(error):
 
 
 if __name__ == "__main__":
-    setup_customers()
-    setup_tracks()
     app.run(debug=True)
